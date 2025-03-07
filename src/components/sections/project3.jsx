9145182d5eebfb0530b2193446054ebfe9ae6c88
angiechang <killmenow107329@gmail.com>
2025-03-07 15:45:37 -0500

    undercon

--- src/components/sections/project3.jsx
+++ src/components/sections/project3.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 export const Project3 = ({ setShowProject }) => {
   return (
-    <div className="min-h-screen bg-black text-white p-10">
+    <div className="min-h-screen bg-[rgb(216,218,215)] text-white p-10">
       <button onClick={() => setShowProject(null)} className="text-blue-400">Back</button>
       <h1 className="text-3xl font-bold">Computer System Projects</h1>
-      <p className="mt-4">Projects related to computer systems and architecture.</p>
+      <p className="mt-4">under construction. Projects related to computer systems and architecture.</p>
     </div>
   );
 };
